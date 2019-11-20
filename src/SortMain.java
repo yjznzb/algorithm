da8f2dc5a36ffbf4f8a32eca527b05c1f57423fb
yjznzb <yjznzb@163.com>
2019-11-20 09:08:33 +0800

    计数排序

--- src/SortMain.java
+++ src/SortMain.java
@@ -3,15 +3,17 @@ import java.util.Random;
 public class SortMain {
 
     public static void main (String[] args) {
-        int[] a = new int[1000];
+        int[] a = new int[10000];
         Random r = new Random(1);
-        for (int i = 0; i < 1000; i++) {
-            a[i] = r.nextInt(1000);
+        for (int i = 0; i < 10000; i++) {
+            a[i] = r.nextInt(100);
         }
         printArray(a);
         //MergeSort ms = new MergeSort();
-        QuickSort qs = new QuickSort();
-        qs.sort(a, 0, 999);
+        //QuickSort qs = new QuickSort();
+        //qs.sort(a, 0, 999);
+        CountingSort cs = new CountingSort();
+        cs.sort(a);
         printArray(a);
     }
 
