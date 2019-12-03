b6ec4693aee110812ef260e139a12534f3bda718
yjznzb <yjznzb@163.com>
2019-12-03 16:58:11 +0800

    根据二叉树前序和中序遍历结果，构建二叉树

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree {
 
@@ -19,6 +17,38 @@ public class BinaryTree {
         result.add(root.val);
         result.addAll(inorderTraversal(root.right));
         return result;
+    }
+
+    /**
+     * 根据二叉树的前序和中序遍历，构建二叉树
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (0 == preorder.length || 0 == inorder.length) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        int index = 0;
+        for (;index < inorder.length; index++) {
+            if (inorder[index] == preorder[0]) {
+                break;
+            }
+        }
+        int[] newPreorder = new int[index];
+        int[] newInorder = new int[index];
+        System.arraycopy(preorder, 1, newPreorder, 0, index);
+        System.arraycopy(inorder, 0, newInorder, 0, index);
+        root.left = buildTree(newPreorder, newInorder);
+
+        newPreorder = new int[preorder.length - index - 1];
+        newInorder = new int[inorder.length - index - 1];
+        System.arraycopy(preorder, index + 1, newPreorder, 0, preorder.length - index - 1);
+        System.arraycopy(inorder, index + 1, newInorder, 0, inorder.length - index - 1);
+        root.right = buildTree(newPreorder, newInorder);
+
+        return root;
     }
 
     public class TreeNode {
